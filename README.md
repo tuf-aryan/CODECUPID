f083a6330d2f07f551548ec72b63f014bac1cd2c
Aryan Gupta <aryangupta56240@gmail.com>
2025-07-13 14:21:30 +0530

    Learn express.js basic Route handling

---
+++ README.md
@@ -0,0 +1,14 @@
+# Express.js Routes Practice
+. 📚 Learning Note: Today I practiced the basics of Express.js, focusing on creating routes, using multiple route handlers, and understanding how  Express middleware works.
+
+# 🚀 What I Learned
+. Setting up a basic Express server
+
+. Creating simple routes (GET, POST)
+
+. Passing multiple route handlers (middleware)
+
+. Sending responses in JSON or plain text
+
+. Understanding the next() function
+
--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
-const express =  require('express');
+const express = require("express");
 
+const app = express();
 
-const app = express(); 
+app.use("/user",(req,res)=>{res.send("hello")});
 
-app.use("/test",(req,res)=>{
-    res.send("helo from server");
-})
-app.use("/hello",(req,res)=>{
-    res.send("helo");
-})
-app.listen(3000,()=>{
-    console.log("server is sucessfully listning on port 3000")
+app.listen(3000, () => {
+  console.log("Server is sucessfully created");
 });
